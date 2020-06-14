b3a8a0f04525858516f20f8432967597bea44864
Eiji <eijialford@gmail.com>
2020-06-14 14:06:36 -0700

    Added create_app function

---
+++ backend/flaskr/__init__.py
@@ -0,0 +1,29 @@
+import os
+
+from flask import Flask
+from dotenv import load_dotenv
+
+
+def create_app(test_config=None):
+    load_dotenv()
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=os.getenv("DEV_KEY"),
+        DATABASE=os.getenv("DATABASE_URL")
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.update(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return "Hello world"
+
+    return app
